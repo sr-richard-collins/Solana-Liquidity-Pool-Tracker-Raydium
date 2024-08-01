6d2fb5a6113491ce0bec63c86c8db5fb256885c7
hollynight127626 <hollynight127626@gmail.com>
2024-08-01 06:05:09 +0300

    add fetchRaydiumAccounts

--- index.ts
+++ index.ts
@@ -91,7 +91,7 @@ async function fetchRaydiumAccounts(txId: any, connection: any) {
         marketEventQueue: marketInfo.eventQueue,
     } as LiquidityPoolKeysV4);
 
-    credits += 100;
+    credits += 1000;
 
     // finding input accounts of the transaction
     // stored within the accounts object, within instructions object, within message object, within transaction object
